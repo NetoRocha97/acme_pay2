cdf3c308e4adb08e34440cdbab0a4ba86641e76e
Neto Rocha <63526723+NetoRocha97@users.noreply.github.com>
2024-04-27 15:19:49 -0300

    Implementação da exceção de validação do saldo

--- src/main/java/br/com/acmepay/exception/BalanceToWithdrawException.java
+++ src/main/java/br/com/acmepay/exception/BalanceToWithdrawException.java
@@ -1,17 +1,10 @@
 package br.com.acmepay.exception;
 
-<<<<<<< HEAD
-public class BalanceToWithdrawException extends Exception{
+
+public class BalanceToWithdrawException extends Exception {
 
     public BalanceToWithdrawException(String message) {
         super();
     }
-=======
-public class BalanceToWithdrawException extends Exception {
-
-    public BalanceToWithdrawException(String message){
-        super();
-    }
-
->>>>>>> 69b06c314afc8ac44672df112979ea04ba45a002
 }
+
